c96c64c2e2addfa787f7734a0096998a96ab7267
YigitYeler <yeleryigit40@gmail.com>
2022-08-12 15:06:24 +0300

    app completed

--- App.js
+++ App.js
@@ -1,20 +1,64 @@
 
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
-import MapView from 'react-native-maps';
-
+import { Marker } from 'react-native-maps';
+import MapView from 'react-native-map-clustering';
+import axios from 'axios';
 export default function App() {
-  return (
-    <MapView
-      initialRegion={{
-        latitude: 37.78825,
-        longitude: -122.4324,
-        latitudeDelta: 0.0922,
-        longitudeDelta: 0.0421
-      }}
-      style={{ height: '100%' }}>
 
-    </MapView>
+  const [data, setData] = useState([])
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    axios.get('https://eonet.gsfc.nasa.gov/api/v2.1/events').then((res) => {
+      setData(res.data.events)
+      setLoading(false)
+    })
+  }, [])
+
+
+  return (
+    <View>
+      {
+        loading ?
+          <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+            <Text style={{ color: 'white' }}>Yükleniyor...</Text>
+          </View>
+          :
+          <MapView
+            initialRegion={{
+              latitude: 52.5,
+              longitude: 19.2,
+              latitudeDelta: 8.5,
+              longitudeDelta: 8.5,
+            }}
+            style={{ height: '100%' }}>
+
+            {
+              data.slice(0, 200).map((item, index) => {
+                return (
+
+                  <Marker
+                    key={item.id}
+                    title={item.title}
+                    description={item.description}
+                    coordinate={{
+                      latitude: item.geometries[0].coordinates[1],
+                      longitude: item.geometries[0].coordinates[0]
+                    }}
+                  >
+                    <View>
+                      <Text style={{ fontSize: 20 }}>🔥</Text>
+                    </View>
+                  </Marker>
+                )
+              })
+            }
+
+          </MapView>
+      }
+
+    </View>
 
   );
 }
